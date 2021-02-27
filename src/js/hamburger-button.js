f62ea2569f5a14b12df4162c99f288824aadcb5f
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2021-02-27 23:13:16 +0100

    Create animation to hamburger button

--- src/js/hamburger-button.js
+++ src/js/hamburger-button.js
@@ -1,3 +1,37 @@
-document.getElementById('hamburger').addEventListener('click', () => {
-   alert('hamburger :)');
-});
+window.onload = () => {
+   const hamburgerButton = document.getElementById('hamburger');
+
+   const hamburgerLineTop = document.getElementById('hamburger-line-top');
+
+   const hamburgerLineMiddle = document.getElementById('hamburger-line-middle');
+
+   const hamburgerLineBottom = document.getElementById('hamburger-line-bottom');
+
+   const hamburgerOpen = () => {
+      hamburgerLineTop.style.transform = 'rotate(45deg)';
+      hamburgerLineTop.style.margin = '-5px auto';
+
+      hamburgerLineMiddle.style.transform = 'translate(-150px)';
+      hamburgerLineMiddle.style.opacity = '0';
+
+      hamburgerLineBottom.style.transform = 'rotate(-45deg)';
+      hamburgerLineBottom.style.margin = '-5px auto';
+   };
+
+   const hamburgerClose = () => {
+      hamburgerLineTop.style.transform = 'rotate(0deg)';
+      hamburgerLineTop.style.margin = '3px auto';
+
+      hamburgerLineMiddle.style.transform = 'translate(0px)';
+      hamburgerLineMiddle.style.opacity = '1';
+
+      hamburgerLineBottom.style.transform = 'rotate(0deg)';
+      hamburgerLineBottom.style.margin = '3px auto';
+   };
+
+   hamburgerButton.addEventListener('click', () => {
+      hamburgerLineMiddle.style.transform === 'translate(-150px)' ?
+          hamburgerClose() :
+          hamburgerOpen();
+   });
+};
