4ee9cfc0879ea55b430dddab47961d0fa2a853df
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2021-04-17 11:28:36 +0200

    Repair scrollback script

--- src/js/scroll-back.js
+++ src/js/scroll-back.js
@@ -1,7 +1,11 @@
 const scrollBack = document.getElementById('scrollBack');
 
-document.addEventListener('scroll', (scroll) => {
+window.onload = () => switchScrollBack(0);
+
+document.addEventListener('scroll', (scroll) => switchScrollBack(scroll));
+
+const switchScrollBack = (scroll) => {
   this.scrollY > window.innerHeight / 2 ?
     scrollBack.style.visibility = 'visible' : 
     scrollBack.style.visibility = 'hidden';
-});
+};
