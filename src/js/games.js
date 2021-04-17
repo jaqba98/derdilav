4fc73cad15f6afa24d02b46ac0358b1e19f5cc61
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2021-04-17 12:15:42 +0200

    Games are load from js script and add information when we dont have any game

---
+++ src/js/games.js
@@ -0,0 +1,21 @@
+const games = [
+  { class: 'flex50', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
+  { class: 'flex50', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
+  { class: 'flex50', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
+  { class: 'flex50', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
+  { class: 'flex50', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
+  { class: 'flex50', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' }
+];
+
+const gameList = document.getElementById('gameList');
+
+if(games.length > 0) {
+  games.forEach(game => {
+    gameList.innerHTML += `<figure class="${game.class}">
+    <img src="${game.src}" alt="${game.name}">
+    <legend>${game.name}</legend>
+  </figure>`;
+  });
+} else {
+  gameList.innerHTML = '<p>Error</p>';
+}
