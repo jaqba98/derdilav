db4f631d48701fdb440b64c53057514b161918a2
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2021-04-26 23:35:32 +0200

    Repaired footer

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,13 +1,10 @@
-const footer = document.getElementById('footer');
-
 window.addEventListener('resize', () => updateFooter());
 
 const updateFooter = () => {
-    const height = window.innerHeight - footer.offsetTop;
-
-    height > 0 ?
-        footer.style.height = height.toString().concat('px') :
-        footer.style.height = 'auto';
+    const footer = document.getElementById('footer');
+    window.innerHeight - footer.offsetTop > 0 ?
+        footer.style.minHeight = height.toString().concat('px') :
+        footer.style.minHeight = 'auto';
 };
 
 updateFooter();
