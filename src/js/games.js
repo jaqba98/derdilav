1faa592aa36b9dc90f06cab042e4c0b356b3c770
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2021-04-26 10:22:56 +0200

    Remove all games

--- src/js/games.js
+++ src/js/games.js
@@ -1,16 +1,18 @@
-const games = [
-  { class: 'flex100', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
-  { class: 'flex50', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
-  { class: 'flex50', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
-  { class: 'flex25', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
-  { class: 'flex25', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
-  { class: 'flex25', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
-  { class: 'flex25', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
-  { class: 'flex25', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
-  { class: 'flex25', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
-  { class: 'flex25', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
-  { class: 'flex25', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' }
-];
+// const games = [
+//   { class: 'flex100', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
+//   { class: 'flex50', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
+//   { class: 'flex50', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
+//   { class: 'flex25', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
+//   { class: 'flex25', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
+//   { class: 'flex25', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
+//   { class: 'flex25', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
+//   { class: 'flex25', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
+//   { class: 'flex25', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
+//   { class: 'flex25', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
+//   { class: 'flex25', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' }
+// ];
+
+const games = [];
 
 const gameList = document.getElementById('gameList');
 
@@ -22,5 +24,5 @@ if(games.length > 0) {
   </figure>`;
   });
 } else {
-  gameList.innerHTML = '<p>Error</p>';
+  gameList.innerHTML = '<p>We not have any game yet 😥</p>';
 }
