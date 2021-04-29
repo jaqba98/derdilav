2f908b908feb716889921be06398fac478c17f95
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2021-04-30 00:10:54 +0200

    Improved team and contact section

--- src/js/games.js
+++ src/js/games.js
@@ -1,32 +1,45 @@
-// const games = [
-//   { class: 'flex100', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
-//   { class: 'flex50', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
-//   { class: 'flex50', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
-//   { class: 'flex25', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
-//   { class: 'flex25', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
-//   { class: 'flex25', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
-//   { class: 'flex25', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
-//   { class: 'flex25', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
-//   { class: 'flex25', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
-//   { class: 'flex25', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
-//   { class: 'flex25', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' }
-// ];
-
-const games = [];
-
-const gameList = document.getElementById('gameList');
-
-if(games.length > 0) {
-  games.forEach(game => {
-    gameList.innerHTML += `<figure class="${game.class}">
-    <img src="${game.src}" alt="${game.name}">
-    <legend>${game.name}</legend>
-  </figure>`;
-  });
-} else {
-  gameList.innerHTML = '<p>We not have any game yet 😥</p>';
-}
-
 class Games {
+  constructor() {
+    this.games = [
+      // { class: 'flex100', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
+      // { class: 'flex50', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
+      // { class: 'flex50', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
+      // { class: 'flex25', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
+      // { class: 'flex25', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
+      // { class: 'flex25', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
+      // { class: 'flex25', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
+      // { class: 'flex25', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
+      // { class: 'flex25', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
+      // { class: 'flex25', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' },
+      // { class: 'flex25', src: 'https://ocdn.eu/images/pulscms/Njk7MDA_/911e000e1049a96a69232a90e0d8f856.jpg', name: 'Wiedźmin 3' }
+    ];
+    this.gameList = document.getElementById('gameList');
+  }
 
+  generate() {
+    this.games.length > 0 ?
+        this.generateGames() :
+        this.generateInformation();
+  }
+
+  generateGames() {
+    this.games.forEach(game => {
+      // noinspection JSUnresolvedVariable
+      this.gameList.innerHTML += `<figure class="${game.class}">
+        <img src="${game.src}" alt="${game.name}">
+        <legend>${game.name}</legend>
+        </figure>`;
+    });
+  }
+
+  generateInformation() {
+    const p = document.createElement('p');
+    const node = document.createTextNode('We not have any game yet');
+    p.appendChild(node);
+    this.gameList.appendChild(p);
+  }
 }
+
+const games = new Games();
+
+games.generate();
--- src/js/hamburger-button.js
+++ src/js/hamburger-button.js
@@ -1,68 +1,66 @@
-window.onload = () => {
-   const hamburgerButton = document.getElementById('hamburger');
-   const hamburgerLineTop = document.getElementById('hamburger-line-top');
-   const hamburgerLineMiddle = document.getElementById('hamburger-line-middle');
-   const hamburgerLineBottom = document.getElementById('hamburger-line-bottom');
-   const menu = document.getElementById('menu');
-   const content = document.getElementById('content');
-   let canCloseMenu = false;
+const hamburgerButton = document.getElementById('hamburger');
+const hamburgerLineTop = document.getElementById('hamburger-line-top');
+const hamburgerLineMiddle = document.getElementById('hamburger-line-middle');
+const hamburgerLineBottom = document.getElementById('hamburger-line-bottom');
+const menu = document.getElementById('menu');
+const content = document.getElementById('content');
+let canCloseMenu = false;
 
-   const hamburgerOpen = () => {
-      hamburgerLineTop.style.transform = 'rotate(45deg)';
-      hamburgerLineTop.style.margin = '-5px auto';
-      hamburgerLineMiddle.style.transform = 'translate(-150px)';
-      hamburgerLineMiddle.style.opacity = '0';
-      hamburgerLineBottom.style.transform = 'rotate(-45deg)';
-      hamburgerLineBottom.style.margin = '-5px auto';
+const hamburgerOpen = () => {
+   hamburgerLineTop.style.transform = 'rotate(45deg)';
+   hamburgerLineTop.style.margin = '-5px auto';
+   hamburgerLineMiddle.style.transform = 'translate(-150px)';
+   hamburgerLineMiddle.style.opacity = '0';
+   hamburgerLineBottom.style.transform = 'rotate(-45deg)';
+   hamburgerLineBottom.style.margin = '-5px auto';
+   menu.style.transform = 'translateX(0%)';
+   content.style.overflow = 'hidden';
+};
+
+const hamburgerClose = () => {
+   hamburgerLineTop.style.transform = 'rotate(0deg)';
+   hamburgerLineTop.style.margin = '3px auto';
+   hamburgerLineMiddle.style.transform = 'translate(0px)';
+   hamburgerLineMiddle.style.opacity = '1';
+   hamburgerLineMiddle.style.zIndex = '0';
+   hamburgerLineBottom.style.transform = 'rotate(0deg)';
+   hamburgerLineBottom.style.margin = '3px auto';
+   menu.style.transform = 'translateX(-100%)';
+   content.style.overflow = 'auto';
+};
+
+hamburgerButton.addEventListener('click', () => {
+   hamburgerLineMiddle.style.transform === 'translate(-150px)' ?
+       hamburgerClose() :
+       hamburgerOpen();
+
+   menu.style.transition = 'transform 1s ease-in-out';
+});
+
+window.addEventListener('resize', () => {
+   if (window.innerWidth > 767) {
+      hamburgerClose();
       menu.style.transform = 'translateX(0%)';
-      content.style.overflow = 'hidden';
-   };
-
-   const hamburgerClose = () => {
-      hamburgerLineTop.style.transform = 'rotate(0deg)';
-      hamburgerLineTop.style.margin = '3px auto';
-      hamburgerLineMiddle.style.transform = 'translate(0px)';
-      hamburgerLineMiddle.style.opacity = '1';
-      hamburgerLineMiddle.style.zIndex = '0';
-      hamburgerLineBottom.style.transform = 'rotate(0deg)';
-      hamburgerLineBottom.style.margin = '3px auto';
+      canCloseMenu = true;
+      menu.style.transition = 'transform 0s ease-in-out';
+   } else if(canCloseMenu) {
       menu.style.transform = 'translateX(-100%)';
-      content.style.overflow = 'auto';
-   };
+      canCloseMenu = false;
+   }
+});
 
-   hamburgerButton.addEventListener('click', () => {
-      hamburgerLineMiddle.style.transform === 'translate(-150px)' ?
-          hamburgerClose() :
-          hamburgerOpen();
-
-      menu.style.transition = 'transform 1s ease-in-out';
-   });
-
-   window.addEventListener('resize', () => {
-      if (window.innerWidth > 767) {
+document.querySelectorAll('#menu > li > a').forEach(item => {
+   item.addEventListener('click', event => {
+      if (window.innerWidth <= 767) {
          hamburgerClose();
-         menu.style.transform = 'translateX(0%)';
-         canCloseMenu = true;
-         menu.style.transition = 'transform 0s ease-in-out';
-      } else if(canCloseMenu) {
          menu.style.transform = 'translateX(-100%)';
          canCloseMenu = false;
       }
    });
+});
 
-   document.querySelectorAll('#menu > li > a').forEach(item => {
-      item.addEventListener('click', event => {
-         if (window.innerWidth <= 767) {
-            hamburgerClose();
-            menu.style.transform = 'translateX(-100%)';
-            canCloseMenu = false;
-         }
-      });
-   });
-
-   setInterval(() => {
-      if (hamburgerLineMiddle.style.transform === 'translate(-150px)') {
-         hamburgerLineMiddle.style.zIndex = '-1';
-      }
-   }, 1000);
-};
+setInterval(() => {
+   if (hamburgerLineMiddle.style.transform === 'translate(-150px)') {
+      hamburgerLineMiddle.style.zIndex = '-1';
+   }
+}, 1000);
