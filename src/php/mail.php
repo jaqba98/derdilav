30c070845e2bb3915953033a49cc0507fafc5646
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2021-05-07 04:24:13 +0200

    Repair how downolad are data from index.html to mail.php

--- src/php/mail.php
+++ src/php/mail.php
@@ -1,12 +1,15 @@
 <?php
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $title = $_POST['title'];
-    $message = $_POST['message'];
-
-    echo 'Name' + $name;
-    echo 'Email' + $email;
-    echo 'Title' + $title;
-    echo 'Message' + $message;
+    echo 'Name: ';
+    echo $_POST['name'];
+    echo '<br>';
+    echo 'Email: ';
+    echo $_POST['email'];
+    echo '<br>';
+    echo 'Title: ';
+    echo $_POST['title'];
+    echo '<br>';
+    echo 'Message: ';
+    echo $_POST['message'];
+    echo '<br>';
     echo '<a href="../index.html">Back</a>';
 ?>
